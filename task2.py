9e969f3e721dd94eb0059e965edc3af0a2f41e05
Dariia <dariiababii02@gmail.com>
2024-01-17 21:22:14 +0100

    search algorithms

---
+++ task2.py
@@ -0,0 +1,43 @@
+def binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+    upper_bound = arr[-1]
+    iterations = 0 
+
+    if upper_bound < x:
+        return -1
+ 
+    while low <= high:
+        iterations += 1
+ 
+        mid = (high + low) // 2
+ 
+        # якщо x більше за значення посередині списку, ігноруємо ліву половину
+        if arr[mid] < x:
+            low = mid + 1
+ 
+        # якщо x менше за значення посередині списку, ігноруємо праву половину
+        elif arr[mid] > x:
+            upper_bound = arr[mid]
+            high = mid - 1
+ 
+        # інакше x присутній на позиції і повертаємо його
+        else:
+            is_exact_match = True  # Встановлюємо прапорець, якщо знайдено точний збіг
+            return mid, iterations
+ 
+    return mid, iterations
+
+arr = [2, 3, 4, 10, 20.5, 35.5, 37.2, 39.1, 41.9, 43, 44, 45, 46]
+x = 37.1
+result, iterations = binary_search(arr, x)
+
+print("Result:", binary_search(arr, x))
+
+if result != -1 and arr[result] == x:
+    print(f"Element is present at index {result}")
+else:
+    print(f"Closest element is present at index {result}")
+
+print(f"Number of iterations: {iterations}")
---
+++ task3.py
@@ -0,0 +1,40 @@
+import timeit
+from typing import Callable
+
+from bm_search import boyer_moore_search
+from kmp import kmp_search
+from rabin_karp import rabin_karp_search
+
+def read_file(filename):
+    with open(filename, 'r', encoding='cp1251') as f:
+        return f.read()
+
+def benchmark(func: Callable, text_: str, pattern_: str):
+    setup_code = f"from __main__ import {func.__name__}"
+    stmt = f"{func.__name__}(text, pattern)"
+    times = timeit.repeat(stmt=stmt, setup=setup_code, globals={'text': text_, 'pattern': pattern_}, repeat=5, number=10)
+    return min(times)
+
+if __name__ == '__main__':
+    filenames = {
+        'стаття 1.txt': {'real': "Жадібний алгоритм", 'fake': "GoIT"},
+        'стаття 2.txt': {'real': "Зв’язний список", 'fake': "GoIT"}
+    }
+
+    results = []
+
+    for filename, patterns in filenames.items():
+        text = read_file(filename)
+        for pattern_type, pattern in patterns.items():
+            time = benchmark(boyer_moore_search, text, pattern)
+            results.append((filename, boyer_moore_search.__name__, pattern_type, time))
+            time = benchmark(kmp_search, text, pattern)
+            results.append((filename, kmp_search.__name__, pattern_type, time))
+            time = benchmark(rabin_karp_search, text, pattern)
+            results.append((filename, rabin_karp_search.__name__, pattern_type, time))
+
+    title = f"{'Файл':<20} | {'Алгоритм':<20} | {'Тип підрядка':<15} | {'Час, сек':<10}"
+    print(title)
+    print("-" * len(title))
+    for result in results:
+        print(f"{result[0]:<20} | {result[1]:<20} | {result[2]:<15} | {result[3]:<10.6f}")
